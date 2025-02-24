7eb1134892447504fea753b55d0f3b2030441a89
PedroRV27 <pedrorovi27@gmail.com>
2025-02-24 20:04:57 +0100

    Estructura MVC con composer

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+const base_url = "http://localhost/TiendaPHP/";
+
+// base de datos
+const SERVIDOR = "localhost";
+const USUARIO = "root";
+const PASSWORD = "";
+const BASE_DATOS = "tienda";
+
+// const controller_default = "Controllers\UsuarioController";
+// const action_default = "index";
+
